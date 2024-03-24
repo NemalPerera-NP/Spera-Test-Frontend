15abc411c0621b9013631f0e9bba2d3463338b56
NemalPerera-NP <nemalperera1999@gmail.com>
2024-03-24 14:08:17 +0530

    registration form for user signup and basic structure of home page completed

---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+function Home(){
+return(
+    <div className="homepage">
+        <h1>Hello {} and Wellcome to Crypto Mart</h1>
+
+    </div>
+)
+}
+export default Home
--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -1,6 +1,62 @@
-// In Signup.js
-import React from "react";
+// In Login.js
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+const Login_url = "http://localhost:8080/api/auth/signup";
+
 function Signup() {
-  return <div>Signup Page</div>;
+  //local states
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+
+  // Function to handle form submission
+  async function submit(e) {
+    e.preventDefault();
+
+    try {
+      await axios.post(loginError, {
+        username,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="Login_Container">
+      <h1>Signup Page</h1>
+
+      <form action="POST">
+        <div className="form-group">
+          <input
+            type="text"
+            id="username"
+            placeholder="UserName"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <input type="submit" onClick={submit} />
+      </form>
+      <br />
+      <p>OR</p>
+      <br />
+      <Link to="/login">Signup Page</Link>
+    </div>
+  );
 }
 export default Signup;
